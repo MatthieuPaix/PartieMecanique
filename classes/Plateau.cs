6b8d19389b89d52f71163278ad87bb91d1a434d3
matthieufelicite <154561282+matthieufelicite@users.noreply.github.com>
2024-02-05 17:44:21 +0100

    Update Plateau.cs

--- classes/Plateau.cs
+++ classes/Plateau.cs
@@ -1,1 +1,13 @@
 
+
+// placer mur(Case, direction) case = case haut à gauche
+
+public class Plateau
+{
+    private Case[][] plateau;
+
+    public Plateau()
+    {
+        plateau = new Case[][];
+    }
+}

90a26220051364fef6bfc8d8c4fde27e6ade7863
matthieufelicite <154561282+matthieufelicite@users.noreply.github.com>
2024-03-18 15:18:14 +0000

    update pawn

--- classes/Pawn.cs
+++ classes/Pawn.cs
@@ -1,27 +1,77 @@
-using System;
+public enum MoveType
+{
+    top,
+    topRight,
+    right,
+    rightBottom,
+    bottom,
+    bottomLeft,
+    left,
+    leftTop
+}
 
 public class Pawn {
+
     private int x;
     private int y;
+
     public Pion(int _x, int _y)
     {
         x = _x;
         y = _y;
     }
+
     public int x 
     {
         get { return x; }
         set { x = value; }
     }
+
     public int y
     {
         get { return y; }
         set { y = value; }
     }
 
-    public void move(int newX, int newY) {
-        x = newX;
-        y = newY;
-    }
+    public void move(MoveType type) 
+    {
+        switch (type)
+        {
+            case top:
+                y += 2;
+                break;
 
+            case topRight:
+                x += 1;
+                y += 1;
+                break;
+
+            case right:
+                x += 2;
+                break;
+
+            case rightBottom:
+                x += 1;
+                y -= 1;
+                break;
+
+            case bottom:
+                y -= 2;
+                break;
+
+            case bottomLeft:
+                x -= 1;
+                y -= 1;
+                break;
+
+            case left:
+                x -= 2;
+                break;
+
+            case leftTop:
+                x -= 1;
+                y += 1;
+                break;
+        }
+    }
 }

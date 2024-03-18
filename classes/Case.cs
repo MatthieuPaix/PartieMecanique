ddf7ca81168aacde80e70ba946dbdbc2f8ab7bf0
matthieufelicite <154561282+matthieufelicite@users.noreply.github.com>
2024-03-18 15:49:14 +0000

    update pawn

--- classes/Case.cs
+++ classes/Case.cs
@@ -1,10 +1,12 @@
-// Import mur
+using Blockade
+
+#include Wall;
 
 public class Case {
     private Wall topWall;
     private Wall leftWall;
-    private Mur rightWall;
-    private Mur bottomWall;
+    private Wall rightWall;
+    private Wall bottomWall;
     private bool StartingCase;
 
     public Case(bool isBeginning){
